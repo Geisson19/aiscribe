d536ef7fa8023d7d889b58724fbcbce77e4780c1
Geisson19 <gs.ponce20@uniandes.edu.co>
2023-09-10 18:09:55 -0500

    fix: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,13 @@
 import * as vscode from "vscode";
-// import { createDoc } from "./extension/functions";
-import { CREATE_DOC_COMMAND, ACTIVATE_COMMAND } from "./extension/commands";
+
+import { ACTIVATE_COMMAND } from "./extension/commands";
 import { activatePanel } from "./extension/panel";
 
 export function activate(context: vscode.ExtensionContext) {
-  // const makeDocCommand = vscode.commands.registerCommand(
-  //   CREATE_DOC_COMMAND,
-  //   createDoc
-  // );
-
   const panelCommand = vscode.commands.registerCommand(ACTIVATE_COMMAND, () => {
     activatePanel(context);
   });
 
-  // context.subscriptions.push(makeDocCommand);
   context.subscriptions.push(panelCommand);
 }
 
--- src/extension/commands/index.ts
+++ src/extension/commands/index.ts
@@ -1,4 +1,1 @@
-export const CREATE_DOC_COMMAND = "aiscribe.createDoc";
 export const ACTIVATE_COMMAND = "aiscribe.activate";
-export const SUMMARIZE_COMMAND = "aiscribe.summarizeSelectedText";
-export const TRANSLATE_COMMAND = "aiscribe.translateToStakeholder";
