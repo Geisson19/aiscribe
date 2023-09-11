c3cbd1017ec30d4163c6ca6d96d9c9944f6e39b5
khcastrillon <kh.castrillon@uniandes.edu.co>
2023-09-10 20:35:36 -0500

    SideBar

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,34 @@
 import * as vscode from "vscode";
+import { SideBarProvider } from './SideBarProvider';
 
 import { ACTIVATE_COMMAND } from "./extension/commands";
 import { activatePanel } from "./extension/panel";
 
 export function activate(context: vscode.ExtensionContext) {
+
+  //Register Sidebar Panel
+  const sideBarProvider = new SideBarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("aiscribe-sidebar", sideBarProvider)
+  );
+
+  // Register a custom command
+	// context.subscriptions.push(vscode.commands.registerCommand('myextension.commandname', () => {
+	// 	// code here...
+	// }));
+  
+  const panel = vscode.window.createWebviewPanel(
+    "chatPanel",
+    "Chat Panel",
+    vscode.ViewColumn.One,
+    {}
+  );
+
+  panel.iconPath = {
+    light: vscode.Uri.file(context.asAbsolutePath("media/icon.svg")),
+    dark: vscode.Uri.file(context.asAbsolutePath("media/icon.svg")),
+  };
+
   const panelCommand = vscode.commands.registerCommand(ACTIVATE_COMMAND, () => {
     activatePanel(context);
   });
