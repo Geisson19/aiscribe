087714b0e9e30f7ecaf9d3b6ed023a6596750f9b
Geisson19 <gs.ponce20@uniandes.edu.co>
2023-09-12 18:09:26 -0500

    prompt command

--- src/extension/functions/runCommands.ts
+++ src/extension/functions/runCommands.ts
@@ -13,37 +13,38 @@ export const runCommandsActive = async () => {
   const text = document.getText();
   console.log(text);
 
-  // Modify the regular expression to match "/prompt" followed by double-quoted text
-  const regex = /\/prompt\s*{([^"]*)}/g;
-  const matches = [...text.matchAll(regex)];
-  console.log(matches);
+  // Get all the lines that starts with /prompt
+  const lines = text.split("\n");
+  const promptLineMatches = lines
+    .filter((line) => line.startsWith("/prompt"))
+    .map((line) => line.replace("/prompt", "").trim());
 
   // For each /prompt command
-  for (const match of matches) {
-    console.log(match);
-    // Get the input
-    const input = match[1];
+  for (const promptLine of promptLineMatches) {
+    console.log(promptLine);
 
     // Get the api response
-    const response = await axios.post(
-      `https://aiscribeapi.azurewebsites.net/?prompt=${input}`
+    const response = await axios.get(
+      `https://aiscribeapi.azurewebsites.net/?prompt=${promptLine}`
     );
 
     const apiResponse = response.data;
-    const apiResponseText = apiResponse.text;
-    const apiResponseTextWithPrompt = `/prompt {${input}}\n${apiResponseText}`;
-    text.replace(match[0], apiResponseTextWithPrompt);
+    const apiResponseText = apiResponse.response.content;
+
+    // Put the api response in the document just below the /prompt command
+    const promptLineIndex = lines.indexOf(promptLine);
+    lines.splice(promptLineIndex + 1, 0, apiResponseText);
   }
 
-  // Replace the current document's text with the new text
-  const fullRange = new vscode.Range(
-    document.positionAt(0),
-    document.positionAt(text.length)
-  );
-
-  editor.edit((editBuilder) => {
-    editBuilder.replace(fullRange, text);
-  });
+  // Update the document with the new text
+  const newText = lines.join("\n");
+  console.log(newText);
+  const firstLine = document.lineAt(0);
+  const lastLine = document.lineAt(document.lineCount - 1);
+  const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+  const edit = new vscode.WorkspaceEdit();
+  edit.replace(document.uri, textRange, newText);
+  await vscode.workspace.applyEdit(edit);
 
   // Save the document
   await document.save();
